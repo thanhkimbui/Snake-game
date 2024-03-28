a4732d76144c88f927429e259811790292c16370
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2024-03-28 07:05:31 -0700

    Store high score into a file

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', 'r') as file:
+            self.high_score = int(file.read())
         self.penup()
         self.color("white")
         self.goto(0, 270)
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', 'w') as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
